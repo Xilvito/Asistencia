09d8bf8650b0588d95cba4300ea78f107f995beb
Diego.Cornejo <diego_ig18@hotmail.com>
2023-09-06 21:51:23 -0300

    nueva

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,55 +1,47 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Animation, AnimationController } from '@ionic/angular';
-import { Hero } from '../hero/hero.component';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  template: `
-  <mat-form-field>
-  <mat-label>Choose a date</mat-label>
-  <input matInput [matDatepicker]="picker">
-  <mat-hint>MM/DD/YYYY</mat-hint>
-  <mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
-  <mat-datepicker #picker></mat-datepicker>
-</mat-form-field>
-
-    <ion-card>
-      <ion-card-content #cardContent>
-        <h1>{{ title }}</h1>
-        <h2>mi heroe favorito: {{ myHero.name }}</h2>
-        <ul>
-          <li *ngFor="let hero of heroes">{{ hero.name }}</li>
-        </ul>
-        <p *ngIf="heroes.length > 3">There are many heroes!</p>
-      </ion-card-content>
-    </ion-card>
-  `,
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  title = 'Tour de Heroes';
-  heroes = [
-    new Hero(1, 'Windstorm'),
-    new Hero(13, 'Bombasto'),
-    new Hero(15, 'Magneta'),
-    new Hero(20, 'Tornado'),
-  ];
+  labelCabecera: any;
+  animacion_labelCabecera: any;
+  animationCtrl: any;
 
-  myHero = this.heroes[2];
+  constructor(private router: Router) {}
 
-  @ViewChild('cardContent', { read: ElementRef, static: false }) cardContent!: ElementRef<HTMLElement>;
-
-  private animation!: Animation;
-
-  constructor(private animationCtrl: AnimationController) {}
-
-  ngAfterViewInit() {
-    this.animation = this.animationCtrl
-      .create()
-      .addElement(this.cardContent.nativeElement)
-      .duration(2500)
-      .iterations(1)
-      .fromTo('transform', 'translateX(20px)', 'translateX(650px)');
-
-    this.animation.play();
+  irAPaginaProfesores() {
+    this.router.navigate(['/login']);
   }
+
+  irAPaginaAlumnos() {
+
+    this.router.navigate(['/login']);
+  };
+
+
+
+ngAfterViewInit() {
+  this.animacion_labelCabecera = this.animationCtrl
+    .create()
+    .addElement(this.labelCabecera.nativeElement)
+    .duration(3000)
+    .iterations(Infinity)
+    /*.fromTo('transform', 'translateX('+px_screem/2+'px)', 'translateX('+px_screem+'px)')
+    .fromTo('opacity', '1', '0.2')*/
+    .keyframes([
+      { offset: 0, transform: 'translateX(0%)'},
+      { offset: 0.5, transform: `translateX(50%)` },
+      { offset: 1, transform: `translateX(100%)` },
+    ])
+    .fromTo('opacity', '1', '0.2');
+
+  this.animacion_labelCabecera.play();
+
+  
+}
+
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,58 +1,85 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder
-} from '@angular/forms';
-import { AlertController } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit {
 
-  formularioLogin: FormGroup;
-  usuario = "fernandez"
-
-  constructor(public fb: FormBuilder,
-    public alertController: AlertController) { 
-
-    this.formularioLogin = this.fb.group({
-      'nombre': new FormControl("",Validators.required),
-      'password': new FormControl("",Validators.required)
-    })
-
+  user={
+    usuario:"nico",
+    password:"1234"
   }
+
+  field:string="";
+
+  constructor(private router: Router,public toastController: ToastController) { }
 
   ngOnInit() {
   }
 
-  async ingresar(){
-    var f = this.formularioLogin.value;
-
-    var usuarioString = localStorage.getItem('usuario');
-    var usuario;
-
-    if (usuarioString !== null) {
-      usuario = JSON.parse(usuarioString);
-    } else {
-      // Maneja el caso en el que 'usuario' no existe en localStorage
+  ingresar(){
+    
+    try{
+      if(this.validateModel(this.user)){
+        let navigationExtras: NavigationExtras = {
+          state: {
+            user: this.user
+          }
+        };
+        
+        this.router.navigate(['/home'],navigationExtras);
+      }
+      else{
+        this.presentToast(this.field);
+      }
+      
     }
-
-    if(usuario.nombre == f.nombre && usuario.password == f.password){
-      console.log('Ingresado');
-    }else{
-      const alert = await this.alertController.create({
-        header: 'Datos incorrectos',
-        message: 'Los datos que ingresaste son incorrectos.',
-        buttons: ['Aceptar']
-      });
-  
-      await alert.present();
+    catch(error){
+      
+      this.router.navigate(['/login']);
     }
+    
+  }
+
+  validateModel(model:any){
+    for (var [key, value] of Object.entries(model)) {
+      if (value=="") {
+        this.field="Falta : "+key;
+        return false;
+      }
+    }
+    
+    var password = model.password;
+    const regex = /^[0-9]*$/;
+    const onlyNumbers = regex.test(password); // true
+    if(!onlyNumbers){
+      this.field = "Password invalida";
+      return false;
+    }
+    else if(model.usuario.length < 3 || model.usuario.length > 8){
+      this.field = "Usuario invalido";
+      return false;
+    }
+    else if(model.password.length != 4){
+      this.field = "Password invalida";
+      return false;
+    }
+    return true;
+  }
+
+  async presentToast(message:string, duration?:number){
+    const toast = await this.toastController.create(
+      {
+        message:message,
+        duration:duration?duration:2000
+      }
+    );
+    toast.present();
   }
 
 }
